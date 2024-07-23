359a8941a2db9c847266cb0394a94fcde0f6f1ce
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2024-07-23 20:54:14 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [ react() ],
+
+plugins: [react()],
+define: {
+
+'process.env.VITE_API_KEY':JSON.stringify(process.env.VITE_API_KEY)
+
+}
 
 })
 
